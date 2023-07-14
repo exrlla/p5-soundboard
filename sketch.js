2be7fe4e7ca8a0512dec72d4a37d5a69f799c50a
estrella <epopo2@illinois.edu>
2023-07-14 09:25:15 -0500

    more

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 function setup() {
-  // put setup code here
+  canvas=createCanvas(w,h);
 }
 
 function draw() {
-  // put drawing code here
+  background(181, 146, 224);
+  noStroke();
+
+  // rectangle 
+  fill(205,92,92);
+  rect((w/4 - 100), (h/2 - 40), 200, 40);
+
+  // ellipse for the top
+  fill(240,128,128);
+  ellipse(w/4, (h/2 - 40), 200, 40);
+
+  fill(205,92,92);
+  // arc(x, y, w, h, start, stop, [mode], [detail])
+  arc(w/4, h/2, 200, 40, TWO_PI, PI);
+
+
+   // rectangle 
+   fill(205,92,92);
+   rect((w/2 - 100), (h/2 - 40), 200, 40);
+ 
+   // ellipse for the top
+   fill(240,128,128);
+   ellipse(w/2, (h/2 - 40), 200, 40);
+ 
+   fill(205,92,92);
+   // arc(x, y, w, h, start, stop, [mode], [detail])
+   arc(w/2, h/2, 200, 40, TWO_PI, PI);
+
+
+
 }
