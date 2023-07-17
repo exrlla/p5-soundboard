a8c067644213041a850ff3d94da54b15ca000bd9
estrella <epopo2@illinois.edu>
2023-07-17 17:22:43 -0500

    add music

--- sketch.js
+++ sketch.js
@@ -4,12 +4,20 @@ let buttons = [];
 let vine_boom;
 let retro_game;
 let whoosh;
+let acute_guitar;
+let guitar_string;
+let guitar_notif;
 
 function setup() {
   canvas=createCanvas(w,h);
   vine_boom = loadSound('vine_boom.mp3');
   retro_game = loadSound('retro_game.wav');
   whoosh = loadSound('whoosh.wav');
+  acute_guitar = loadSound('acute_guitar.wav');
+  guitar_string = loadSound('guitar_string.wav');
+  guitar_notif = loadSound('guitar_notif.wav');
+
+
 
   let b1 = new Button(w/4, h/2, 200, 40, color(205,92,92), color(240,128,128), vine_boom);
   let b2 = new Button(w/2, h/2, 200, 40, color(72,209,204), color(175,238,238), retro_game);
@@ -19,9 +27,9 @@ function setup() {
   let b5 = new Button(w/2, h/3, 200, 40, color(0, 159, 183), color(153, 241, 255), retro_game);
   let b6 = new Button(w/4*3, h/3, 200, 40, color(145, 145, 233), color(204, 204, 245), whoosh);
 
-  let b7 = new Button(w/4, 2*h/3, 200, 40, color(255, 163, 175), color(255, 214, 220), vine_boom);
-  let b8 = new Button(w/2, 2*h/3, 200, 40, color(143, 45, 86), color(216, 131, 166), retro_game);
-  let b9 = new Button(w/4*3, 2*h/3, 200, 40, color(236, 78, 32), color(234, 162, 133), whoosh);
+  let b7 = new Button(w/4, 2*h/3, 200, 40, color(255, 163, 175), color(255, 214, 220), acute_guitar);
+  let b8 = new Button(w/2, 2*h/3, 200, 40, color(143, 45, 86), color(216, 131, 166), guitar_string);
+  let b9 = new Button(w/4*3, 2*h/3, 200, 40, color(236, 78, 32), color(234, 162, 133), guitar_notif);
   
   buttons.push(b1);
   buttons.push(b2);
