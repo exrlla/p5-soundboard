e5103c2963d09830ff16a54f5a9da1fa529c5d49
estrella <epopo2@illinois.edu>
2023-07-20 10:44:08 -0500

    change w and h vars

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-var w = window.innerWidth;
-var h = window.innerHeight;
+let width = window.innerWidth;
+let height = window.innerHeight;
 
 let buttons_top = [];
 let buttons_middle = [];
@@ -13,7 +13,7 @@ let guitar_string;
 let guitar_notif;
 
 function setup() {
-  canvas=createCanvas(w,h);
+  canvas=createCanvas(width,height);
   vine_boom = loadSound('vine_boom.mp3');
   retro_game = loadSound('retro_game.wav');
   whoosh = loadSound('whoosh.wav');
@@ -23,17 +23,17 @@ function setup() {
 
 
 
-  let b1 = new Button(w/3, h/3, 200, 40, color(226, 132, 19), color(244, 190, 124), vine_boom);
-  let b2 = new Button(w/2, h/3, 200, 40, color(0, 159, 183), color(153, 241, 255), retro_game);
-  let b3 = new Button(2*w/3, h/3, 200, 40, color(145, 145, 233), color(204, 204, 245), whoosh);
+  let b1 = new Button(width/3, height/3, 200, 40, color(226, 132, 19), color(244, 190, 124), vine_boom);
+  let b2 = new Button(width/2, height/3, 200, 40, color(0, 159, 183), color(153, 241, 255), retro_game);
+  let b3 = new Button(2*width/3, height/3, 200, 40, color(145, 145, 233), color(204, 204, 245), whoosh);
 
-  let b4 = new Button(w/3, h/2, 200, 40, color(205,92,92), color(240,128,128), vine_boom);
-  let b5 = new Button(w/2, h/2, 200, 40, color(72,209,204), color(175,238,238), retro_game);
-  let b6 = new Button(2*w/3, h/2, 200, 40, color(255,215,0), color(255,255,0), whoosh);
+  let b4 = new Button(width/3, height/2, 200, 40, color(205,92,92), color(240,128,128), vine_boom);
+  let b5 = new Button(width/2, height/2, 200, 40, color(72,209,204), color(175,238,238), retro_game);
+  let b6 = new Button(2*width/3, height/2, 200, 40, color(255,215,0), color(255,255,0), whoosh);
 
-  let b7 = new Button(w/3, 2*h/3, 200, 40, color(255, 163, 175), color(255, 214, 220), acute_guitar);
-  let b8 = new Button(w/2, 2*h/3, 200, 40, color(143, 45, 86), color(216, 131, 166), guitar_string);
-  let b9 = new Button(2*w/3, 2*h/3, 200, 40, color(236, 78, 32), color(234, 162, 133), guitar_notif);
+  let b7 = new Button(width/3, 2*height/3, 200, 40, color(255, 163, 175), color(255, 214, 220), acute_guitar);
+  let b8 = new Button(width/2, 2*height/3, 200, 40, color(143, 45, 86), color(216, 131, 166), guitar_string);
+  let b9 = new Button(2*width/3, 2*height/3, 200, 40, color(236, 78, 32), color(234, 162, 133), guitar_notif);
   
   buttons_top.push(b1);
   buttons_top.push(b2);
@@ -59,9 +59,9 @@ function mousePressed() {
 
 function mouseReleased() {
   for (let i = 0; i < buttons_top.length; i++) {
-    buttons_top[i].y = h/3;
-    buttons_middle[i].y = h/2;
-    buttons_bottom[i].y = 2*h/3;
+    buttons_top[i].y = height/3;
+    buttons_middle[i].y = height/2;
+    buttons_bottom[i].y = 2*height/3;
   }
 }
 
